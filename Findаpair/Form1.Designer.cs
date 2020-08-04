021e244c978d7478aa18af732e43f7275d439b83
Max <maxima-2000@mail.ru>
2020-08-04 23:19:11 +0300

    Add project files.

---
+++ Findаpair/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Findаpair
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 6;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(6, 67);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(648, 319);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Количество попыток: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(127, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 395);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MaximumSize = new System.Drawing.Size(677, 434);
+            this.MinimumSize = new System.Drawing.Size(677, 434);
+            this.Name = "Form1";
+            this.Text = "Игра \"Найди пару\"";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Findаpair/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Findаpair
+{
+    
+    public partial class Form1 : Form
+    {
+        private int n = 6;
+        private int count = 0;
+        private int countTrue = 0;
+        private int indicator= 0;
+        private Label secondLabel;
+        private Label firstLabel;
+        private List<string> Elems1 = new List<string> { "A", "B", "C", "D", "Y", "G", "K", "L", "M", "N", "Q", "T", "U", "X", "Z", "H", "F", "V", "A", "B", "C", "D", "Y", "G", "K", "L", "M", "N", "Q", "T", "U", "X", "Z", "H", "F", "V" };
+        Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            tableFiller(Elems1);
+        }
+
+        //Заполняем таблицу случайными элементами
+        private void tableFiller(List<string> Elems)
+        {
+            List<string> ElemsN = new List<string>();
+            ElemsN.AddRange(Elems);
+            for (int i = 0; i <= 35; i++)
+            {
+                Panel panel = new Panel();
+                panel.BorderStyle = BorderStyle.FixedSingle;
+                panel.Dock = DockStyle.Fill;
+
+                Label label = new Label();
+                label.Dock = DockStyle.Fill;
+                label.Visible = true;
+                label.BackColor = Color.Black;
+                label.TextAlign = ContentAlignment.MiddleCenter;
+                label.Font = new Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+                int buf = rand.Next(0, ElemsN.Count);
+                label.Text = ElemsN[buf];
+                label.Click += new EventHandler(label_Click);
+                ElemsN.RemoveAt(buf);
+                panel.Controls.Add(label);
+                tableLayoutPanel1.Controls.Add(panel);
+            }
+        }
+        private void label_Click(object sender, EventArgs e)
+        {
+            Label clicedLabel = sender as Label;
+
+            if(countTrue == 18)
+            {
+                MessageBox.Show("Поздравляем, вы нашли все пары ! \b Чтобы начать заного перезапустите программу.");
+            }
+            if (clicedLabel.BackColor == Color.White || clicedLabel.BackColor == Color.LightGreen)
+            {
+                return;
+            }
+
+            indicator++;
+
+            if (indicator == 2)
+            {
+                label2.Text = Convert.ToString(++count);
+                secondLabel = clicedLabel;
+                if (firstLabel.Text == secondLabel.Text)
+                {
+                    countTrue++;
+                    
+                    indicator = 0;
+                    firstLabel.BackColor = Color.LightGreen;
+                    secondLabel.BackColor = Color.LightGreen;
+                    firstLabel = null;
+                    secondLabel = null;
+                    if (countTrue == 18)
+                    {
+                        MessageBox.Show("Поздравляем, вы нашли все пары ! Чтобы начать заного перезапустите программу.");
+                    }
+                    return;
+                }
+                else
+                {
+                    secondLabel.BackColor = Color.White;
+                    return;
+                }
+            }
+            if( indicator == 3)
+            {
+                indicator = 1;
+                firstLabel.BackColor = Color.Black;
+                if (secondLabel != null)
+                {
+                    secondLabel.BackColor = Color.Black;
+                }
+                secondLabel = null;
+                firstLabel = clicedLabel;
+                firstLabel.BackColor = Color.White;
+                return;
+            }
+            //Если все хорошо
+            clicedLabel.BackColor = Color.White;
+            firstLabel = clicedLabel;
+        }
+    }
+}
